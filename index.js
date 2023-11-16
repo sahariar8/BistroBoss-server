8e1455cb33e1a175092d3740d8f9a56409935024
Sahariar <sahariaralam8@gmail.com>
2023-11-17 03:49:35 +0600

    doing well

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import jwt  from  'jsonwebtoken'
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
 
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
@@ -26,6 +27,42 @@ async function run() {
       const menuCollection = client.db('bistrobossDB').collection('menu');
       const reviewCollection = client.db('bistrobossDB').collection('reviews');
       const cartCollection = client.db('bistrobossDB').collection('carts');
+      const userCollection = client.db('bistrobossDB').collection('users');
+
+    //middleware
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn:'1h' });
+      res.send({token});
+    })
+
+
+      const verifyToken = (req,res,next) =>{
+        console.log('Inside Verify Token',req.headers.authorization);
+        if(!req.headers.authorization){
+          return res.status(401).send({ message:'UnAuthorized Access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, ( err, decoded)=>{
+          if(err){
+            return res.status(401).send({ message:'UnAuthorized Access' });
+          }
+          req.decoded = decoded;
+          next();
+        })
+        
+      }
+
+      const verifyAdmin = async(req,res,next) =>{
+          const email = req.decoded.email;
+          const query = { email :email };
+          const user = await userCollection.findOne(query);
+          const isAdmin = user?.role === 'admin';
+          if(!isAdmin){
+            return res.status(403).send({message : 'foebidden access'})
+          }
+          next();
+      }
 
       app.get('/menu',async(req,res)=>{
         const query = menuCollection.find();
@@ -63,6 +100,62 @@ async function run() {
         res.send(result);
       })
 
+      app.post('/users',async (req,res)=>{
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+          return res.send({message:'user Already Exist', insertedId : null })
+        }
+
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      })
+
+      app.get('/users',verifyToken,verifyAdmin, async(req,res)=>{
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      })
+
+      app.delete('/users/:id',verifyToken,verifyAdmin,async(req,res)=>{
+        const id = req.params.id;
+        const query = { _id :new ObjectId(id)};
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
+      })
+
+      app.patch('/users/admin/:id',verifyToken,verifyAdmin, async(req,res)=>{
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set:{
+              role:"admin"
+          }
+        }
+        const result = await userCollection.updateOne(filter,updatedDoc);
+        res.send(result);
+      })
+
+      app.get('/users/admin/:email',verifyToken, async(req,res)=>{
+          const email = req.params.email;
+          if(email !== req.decoded.email){
+            return res.status(403).send({mssage:'forbidden access'});
+          }
+          const query = { email: email };
+          const user = await userCollection.findOne(query);
+          let admin = false;
+          if(user){
+            admin = user?.role ==='admin';
+          }
+          res.send({admin});
+      })
+
+      //MIDDLEWARE
+
+  
+      //token related route
+
+     
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
